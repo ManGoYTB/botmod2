8c8f5cede586c18cefbc819096c45716fb2484e1
ManGoYTB <mangoytbfr@gmail.com>
2017-11-25 12:27:22 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -433,6 +433,17 @@ class Modmail(commands.Bot):
             await roleObj.edit(colour=discord.Colour(value=colour))
             await asyncio.sleep(interval)
             await self.say(embed=yes)
+		
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def servers(self, ctx):
+        '''Listet die aktuellen verbundenen Server auf (BOT OWNER ONLY)'''
+        msg = '```js\n'
+        msg += '{!s:19s} | {!s:>5s} | {} | {}\n'.format('ID', 'Member', 'Name', 'Owner')
+        for server in self.bot.servers:
+            msg += '{!s:19s} | {!s:>5s}| {} | {}\n'.format(server.id, server.member_count, server.name, server.owner)
+        msg += '```'
+        await self.bot.say(msg)
 	
 if __name__ == '__main__':
     Modmail.init()
