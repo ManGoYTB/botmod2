c351f4e64b657be550aa9f5feb82002324178330
ManGoYTB <mangoytbfr@gmail.com>
2017-11-25 11:48:49 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -445,6 +445,34 @@ class Modmail(commands.Bot):
             colour = int(colour, 16)
             await roleObj.edit(colour=discord.Colour(value=colour))
             await asyncio.sleep(interval)
+		
+    @commands.command(no_pm=True, pass_context=True, name="massdm", aliases=["mdm"])
+    @commands.has_permissions(administrator=True)
+    async def _mdm(self, ctx: commands.Context,
+                   role: discord.Role, *, message: str):
+        """Sends a DM to all Members with the given Role.
+        Allows for the following customizations:
+        {0} is the member being messaged.
+        {1} is the role they are being message through.
+        {2} is the person sending the message.
+        """
+ 
+        server = ctx.message.server
+        sender = ctx.message.author
+ 
+        try:
+            await self.bot.delete_message(ctx.message)
+        except:
+            pass
+ 
+        dm_these = self._get_users_with_role(server, role)
+ 
+        for user in dm_these:
+            try:
+                await self.bot.send_message(user,
+                                            message.format(user, role, sender))
+            except (discord.Forbidden, discord.HTTPException):
+                continue
 	
 if __name__ == '__main__':
     Modmail.init()
