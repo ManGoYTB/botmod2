ff8e3be129b915f331cde7b50902dc16a9fdf89e
ManGoYTB <mangoytbfr@gmail.com>
2018-03-07 01:12:30 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -141,11 +141,11 @@ class Modmail(commands.Bot):
         cmds = '`m.setup [modrole] <- (optional)` - Commande qui configure le bot. **Bot Owner Only**\n' \
                '`m.reply <message...>` - Envoie un message au destinataire du thread en cours.\n' \
                '`m.close` - Ferme le fil en cours et supprime le canal.\n' \
-               '`m.disable` - Ferme tous les threads et désactive les mails pour le serveur. **Bot Owner Only*\n' \
+               '`m.disable` - Ferme tous les threads et désactive les mails pour le serveur. **Bot Owner Only**\n' \
                '`m.customstatus` - Définit le statut du Bot sur ce que vous voulez. **Bot Owner Only**'
 
         em.add_field(name='Commands', value=cmds)
-        em.add_field(name='Owner ManGo', value='ManGo#7532')
+        em.add_field(name='Owner ManGo', value='<@419158190786347008>')
         em.set_footer(text='Dm ManGo Pour Avoir Des Aides !')
 
         return em
