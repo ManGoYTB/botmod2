e7cf5853ccde6547c1fe9622d34fb5c6cb238c23
ManGoYTB <mangoytbfr@gmail.com>
2018-11-30 23:23:16 +0100

    Update checks.py

--- utils/checks.py
+++ utils/checks.py
@@ -3,6 +3,6 @@ from discord.ext import commands
 
 def is_owner():
     def is_owner_check(message):
-        return message.author.id == '193040053348466690'
+        return message.author.id == '475703972393648139'
 
     return commands.check(lambda ctx: is_owner_check(ctx.message))
